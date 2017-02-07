047c72f56f82a526ca844d21ee05bc70f18f1fc2
Tsukihub <audeeccher@gmail.com>
2017-02-07 14:28:30 +0100

    contrôle séparé

--- index.php
+++ index.php
@@ -5,6 +5,25 @@
  $tabPhp = array("img/ane.jpg", "img/ane.jpg");
 shuffle($tabPhp);//shuffle($array)
 // var_dump($tabPhp);//affiche tt le contenu du tableau en html
+
+
+$my_file = 'file.txt';
+$handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
+
+
+if (isset($_GET["min"], $_GET["sec"], $_GET["nom"])){
+	    $min=$_GET["min"];
+	    $sec=$_GET["sec"];
+	    $etat="victoire";
+	    $nom=$_GET["nom"];
+	    $new_data = "\n".$min.", min ".$sec.", sec ".$nom;
+		fwrite($handle, $new_data);
+}else{$etat="jeu";
+}
+
+
+/////////////////////////////////////////php de contrôle//////////////////////////////////////////////////
+
 ?>	
 <script type="text/javascript">
 	<?php 
@@ -20,17 +39,7 @@ shuffle($tabPhp);//shuffle($array)
  // $dernierecle=array_pop(array_keys($array));//gets the last key inside of dernierecle
  
 	echo "var tab=[$tableau];";
-	    if(isset($_GET["min"])){    
-        $min=$_GET["min"];
-		}else{$temps='';
-		}
-		echo $temps;
-		if(isset($_GET["sec"])){    
-        $sec=$_GET["sec"];
-    }
-    if(isset($_GET["nom"])){    
-        $nom=$_GET["nom"];
-    }
+
 	?>
 </script>
 <!DOCTYPE html>
@@ -46,16 +55,20 @@ shuffle($tabPhp);//shuffle($array)
 		<div id="titre">
 		<h1>JEU DES PAIRES</h1>
 		</div>
-		<p>Règles du jeu: Afficher toutes les paires pour gagner</p>
-		<p>Vous avez trouvé <span id="paires">0</span> paires cachées</p>
-		<span id="chronotime">00:00</span>
+	
+	    <?php
+	    if ($etat=="jeu") {
+	      	echo"		<p>Règles du jeu: Afficher toutes les paires pour gagner</p>
+		<p>Vous avez trouvé <span id='paires'>0</span> paires cachées</p><span id='chronotime'>00:00</span>";
+	      }  
 		
+		?>
 			
 		 <div id="photo"> 
 		  <!--ici javascript--> 
 		  <?php
 //////////////////////////////////////////////////fonction afficher/////////////////////////////////////////	   
-		  if(isset($_GET["min"])){ 
+		  if($etat=="victoire"){ 
 	echo '<h1> Vous avez gagné !</h1><br /><div class="boutton"><input type="button" class="restart" value="Recommencer" onClick="restart()"></div>';
 	echo "vous avez gagné en $min:$sec, $nom";
 }else{
